769e7134faa629028561f8a13cbbea5032b8d723
sevdas <sshukrieva@gmail.com>
2021-06-01 11:39:26 +0100

    add-database
    Co-authored-by: Amy <amykuner1@gmail.com>

---
+++ database/init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "users" (
+  "id" int PRIMARY KEY,
+  "name" text,
+  "email" varchar UNIQUE,
+  "password" varchar,
+  "gender" varchar
+);
+
+CREATE TABLE "languages" (
+  "user_id" int,
+  "language" text,
+  "proficiency" text
+);
+
+CREATE TABLE "connections" (
+  "user_id" int,
+  "woman" boolean,
+  "man" boolean,
+  "non_binary" boolean,
+  "anyone" boolean
+);
+
+CREATE TABLE "sessions" (
+  "sid" text,
+  "data" json
+);
+
+ALTER TABLE "languages" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "connections" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
