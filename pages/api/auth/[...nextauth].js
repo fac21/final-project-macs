dd36b7d696a166610c2af51ac33c504f874f8c8e
sevdas <sshukrieva@gmail.com>
2021-06-02 14:56:21 +0100

    add authentication library, Refers #2
    Co-authored by:  Maryam Ghorbani <ghorbani.maryam@gmail.com>

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+const options = {
+  site: process.env.DATABASE_URL,
+  providers: [
+    // Providers.Email({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // }),
+    Providers.Email({
+      server: {
+        port: 465,
+        host: "smtp.gmail.com", // create a gmail address and activate less secure app access on google
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  database: process.env.DATABASE_URL,
+};
+
+export default (req, res) => NextAuth(req, res, options);
+
+console.dir(Providers);
