b3f59c01da01c41a5ba94b11ddc467ed25023ba0
Amy Kuner <amykuner1@gmail.com>
2021-06-03 10:11:46 +0100

    server working

--- pages/profiles/connect/[user].js
+++ pages/profiles/connect/[user].js
@@ -7,72 +7,112 @@ import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
 const socket = socketIOClient(ENDPOINT);
 
-export default function Connect(){
-    
-    const [user, setUser] = useState({
-        usersList: null
-      });
-      const [msg, setMsg] = useState("");
-      const [recMsg, setRecMsg] = useState({
-        listMsg: []
-      });
-      const [loggedUser, setLoggedUser] = useState();
-    console.log(loggedUser)
-      useEffect(() => {
-        // subscribe a new user
-        socket.emit("login", "username");
-        // list of connected users
-        socket.on("users", data => {
-          setUser({ usersList: JSON.parse(data) })
-        });
-          // get the logged user
-      socket.on("connecteduser", data => {
-        setLoggedUser(JSON.parse(data));
-      });
-    
-        // we get the messages
-        socket.on("getMsg", data => {
-          let listMessages = recMsg.listMsg;
-          //add new message into array
-          listMessages.push(JSON.parse(data));
-          //update received message to be the list of messages
-          setRecMsg({ listMsg: listMessages });
-        });
-      }, []);
-    
-      // to send a message
-      const sendMessage = () => {
-        socket.emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: msg }));
-      }
-        
-    return <Layout> 
-         <div>
-      <h3 className="d-flex justify-content-center"> Connected users : {user.usersList?.length} </h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th> User name </th>
-            <th> Connection Date </th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.usersList?.map(user => {
-            return (<tr key={user.id}>
-              <td> {user.userName} </td>
-              <td> {user.connectionTime} </td>
-            </tr>)
+export default function Connect() {
+  const [user, setUser] = useState({
+    usersList: null,
+  });
+  const [msg, setMsg] = useState("");
+  const [recMsg, setRecMsg] = useState({
+    listMsg: [],
+  });
+  const [loggedUser, setLoggedUser] = useState();
+  console.log(loggedUser);
+  useEffect(() => {
+    // subscribe a new user
+    socket.emit("login", "username");
+    // list of connected users
+    socket.on("users", (data) => {
+      setUser({ usersList: JSON.parse(data) });
+      console.log("THIS IS WORKING");
+    });
+    // get the logged user
+    socket.on("connecteduser", (data) => {
+      setLoggedUser(JSON.parse(data));
+    });
+
+    // we get the messages
+    socket.on("getMsg", (data) => {
+      let listMessages = recMsg.listMsg;
+      //add new message into array
+      listMessages.push(JSON.parse(data));
+      //update received message to be the list of messages
+      setRecMsg({ listMsg: listMessages });
+    });
+  }, []);
+
+  // to send a message
+  const sendMessage = () => {
+    socket.emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: msg }));
+  };
+
+  return (
+    <Layout>
+      <div>
+        <h3 className="d-flex justify-content-center">
+          {" "}
+          Connected users : {user.usersList?.length}{" "}
+        </h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th> User name </th>
+              <th> Connection Date </th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.usersList?.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td> {user.userName} </td>
+                  <td> {user.connectionTime} </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <h3 className="d-flex justify-content-center">
+          {" "}
+          User : {loggedUser?.userName}{" "}
+        </h3>
+        <div style={{ borderStyle: "inset" }}>
+          <h2 className="d-flex justify-content-center"> Chat </h2>
+          {recMsg.listMsg?.map((msgInfo, index) => {
+            return (
+              <div className="d-flex justify-content-center" key={index}>
+                {" "}
+                <b>{msgInfo.userName} </b> : {msgInfo.msg}{" "}
+                <small
+                  style={{
+                    marginLeft: "18px",
+                    color: "blue",
+                    marginTop: "5px",
+                  }}
+                >
+                  {" "}
+                  {msgInfo.time}{" "}
+                </small>{" "}
+              </div>
+            );
           })}
-        </tbody>
-      </table>
-      <h3 className="d-flex justify-content-center"> User : {loggedUser?.userName} </h3>
-      <div style={{ borderStyle: "inset" }}>
-        <h2 className="d-flex justify-content-center"> Chat </h2>
-        {recMsg.listMsg?.map((msgInfo, index) => { return (<div className="d-flex justify-content-center" key={index}> <b>{msgInfo.userName} </b> :  {msgInfo.msg} <small style={{ marginLeft: "18px", color: "blue", marginTop: "5px" }}> {msgInfo.time} </small> </div>) })}
+        </div>
+        <div className="d-flex justify-content-center">
+          <input
+            style={{ width: "300px", display: "inline" }}
+            id="inputmsg"
+            onChange={(event) => setMsg(event.target.value)}
+          />
+          <button
+            className="btn btn-info"
+            id="btnmsg"
+            onClick={() => {
+              sendMessage();
+            }}
+          >
+            {" "}
+            Send{" "}
+          </button>
+        </div>
       </div>
-      <div className="d-flex justify-content-center">
-        <input style={{ width: "300px", display: "inline" }} id="inputmsg" onChange={(event) => setMsg(event.target.value)} />
-        <button className="btn btn-info" id="btnmsg" onClick={() => { sendMessage() }}> Send </button>
-      </div>
-    </div >
     </Layout>
+  );
 }
