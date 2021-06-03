f8ac47002b9c2c0bd04548832dfa0973b70ae8ed
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-06-03 14:31:19 +0100

    modified init.sql

--- database/init.sql
+++ database/init.sql
@@ -2,18 +2,24 @@ BEGIN;
 
 DROP TABLE IF EXISTS users, languages, connections, sessions CASCADE;
 CREATE TABLE "users" (
-  "id" int PRIMARY KEY,
+"id" int PRIMARY KEY,
   "name" text,
-  "email" varchar UNIQUE,
-  "password" varchar,
-  "gender" varchar
+  "email" VARCHAR ( 255 ) UNIQUE NOT NULL,
+  "password" VARCHAR ( 255 ) NOT NULL,
+  "gender" varchar,
+  "image" text,
+  "created_at" timestamp,
+  "updated_at" timestamp,
+  "email_verified" TIMESTAMPTZ,
 );
+
 
 CREATE TABLE "languages" (
   "user_id" int,
   "language" text,
   "proficiency" text
 );
+
 
 CREATE TABLE "connections" (
   "user_id" int,
@@ -28,7 +34,15 @@ CREATE TABLE "sessions" (
   "data" json
 );
 
+
+INSERT INTO users (id, name, email, password, gender, email_verified, image, created_at, updated_at) VALUES
+  (1, 'maryam', 'ghorbani.maryam6688@gmail.com', 'me2293403', 'female', (SELECT CURRENT_TIMESTAMP),'', (SELECT CURRENT_TIMESTAMP), (SELECT CURRENT_TIMESTAMP))
+;
 ALTER TABLE "languages" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+
+
+
 
 ALTER TABLE "connections" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
