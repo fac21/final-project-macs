2032be0839daddfc085b85892833471eee231671
Amy Kuner <amykuner1@gmail.com>
2021-06-08 12:26:56 +0100

    websocket works for separate rooms

--- chatserver.js
+++ chatserver.js
@@ -18,28 +18,24 @@ const io = socketIo(server, {
 });
 
 let users = [];
-io.on("connectionToRoom", (socket) => {
-  socket.on("room", function (room) {
-    socket.join(room);
-    socket.on("login", (userName) => {
-      users.push({
-        id: socket.id,
-        userName: userName,
-        connectionTime: new moment().format("YYYY-MM-DD HH:mm:ss"),
-      });
-      socket
-        .to(room)
-        .emit("connecteduser", JSON.stringify(users[users.length - 1]));
-      io.in(room).emit("users", JSON.stringify(users));
+io.on("connection", (socket) => {
+  socket.on("joinRoom", (roomID) => {
+    console.log("room joined");
+    socket.join(roomID);
+    users.push({
+      id: socket.id,
+      // userName: userName,
+      connectionTime: new moment().format("YYYY-MM-DD HH:mm:ss"),
     });
+
     socket.on("sendMsg", (msgTo) => {
-      msgTo = JSON.parse(msgTo);
+      console.log("recieve message", msgTo);
       const minutes = new Date().getMinutes();
-      io.in(room).emit(
-        "getMsg",
+      io.to(roomID).emit(
+        "message",
         JSON.stringify({
           id: socket.id,
-          userName: users.find((e) => e.id == msgTo.id).userName,
+          //userName: users.find((e) => e.id == msgTo.id).userName,
           msg: msgTo.msg,
           time:
             new Date().getHours() +
@@ -47,6 +43,10 @@ io.on("connectionToRoom", (socket) => {
             (minutes < 10 ? "0" + minutes : minutes),
         })
       );
+    });
+    socket.on("leaveRoom", () => {
+      socket.offAny("sendMsg");
+      socket.leave(roomID);
     });
   });
 
@@ -56,7 +56,7 @@ io.on("connectionToRoom", (socket) => {
       index = users.findIndex((e) => e.id == socket.id);
     }
     if (index >= 0) users.splice(index, 1);
-    io.in(room).emit("users", JSON.stringify(users));
+    io.emit("users", JSON.stringify(users));
   });
 });
 
--- pages/profiles/connect/[chatString].js
+++ pages/profiles/connect/[chatString].js
@@ -10,11 +10,12 @@ import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
 const socket = socketIOClient(ENDPOINT);
 
-export default function Connect() {
+export default function Connect(props) {
   const room = props.chatString;
-  socket.on("connectToRoom", function () {
-    socket.emit("room", room);
-  });
+  // socket.on("connection", function () {
+  //   console.log("connected");
+  //   socket.emit("room", room);
+  // });
 
   const [user, setUser] = useState({
     usersList: null,
@@ -23,21 +24,23 @@ export default function Connect() {
   const [recMsg, setRecMsg] = useState({
     listMsg: [],
   });
-  const [loggedUser, setLoggedUser] = useState("logged in user");
+  // const [loggedUser, setLoggedUser] = useState("logged in user");
   useEffect(() => {
     // subscribe a new user
-    socket.to(room).emit("login", "username");
+    // socket.emit("login", "username");
     // list of connected users
     socket.on("users", (data) => {
       setUser({ usersList: JSON.parse(data) });
     });
     // get the logged user
-    socket.on("connecteduser", (data) => {
-      setLoggedUser(JSON.parse(data));
-    });
+    // socket.on("connecteduser", (data) => {
+    //   setLoggedUser(JSON.parse(data));
+    // });
+
+    socket.emit("joinRoom", room);
 
     // we get the messages
-    socket.on("getMsg", (data) => {
+    socket.on("message", (data) => {
       let listMessages = recMsg.listMsg;
       //add new message into array
       listMessages.push(JSON.parse(data));
@@ -48,18 +51,22 @@ export default function Connect() {
 
   // to send a message
   const sendMessage = () => {
-    socket
-      .to(room)
-      .emit("sendMsg", JSON.stringify({ id: loggedUser.id, msg: msg }));
+    socket.emit("sendMsg", { id: "Cragalag", msg });
   };
+
+  useEffect(() => {
+    return () => {
+      socket.emit("leaveRoom");
+    };
+  }, []);
 
   return (
     <>
       <Header />
       <Layout>
         <div>
-          <h3> Connected users : {user.usersList?.length} </h3>
-          <h3> User : {loggedUser?.userName} </h3>
+          {/* <h3> Connected users : {user.usersList?.length} </h3> */}
+          {/* <h3> User : {loggedUser?.userName} </h3> */}
           <div>
             <h2> Chat </h2>
             {recMsg.listMsg?.map((msgInfo, index) => {
