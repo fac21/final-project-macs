bf341d3492bb581234acf3941f101952ac2ba1f4
Craig MacLeod <c92macleod@gmail.com>
2021-06-10 15:10:26 +0100

    styled error pages

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,43 @@
 import Link from "next/link"
+import Header from "../components/HomeHeader";
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import Logo from "../components/Logo"
+import AccessDeniedHeader from "../components/AccessDeniedHeader"
 
-const notFound = () => {
+export default function notFound() {
+
     return (
-        <div>
-        <h1>Ooops...</h1>
-        <h2>That page can not be found!</h2>
-        <p>Go back to the 
-        <Link href="/">
-        <a>Home</a>
-        </Link>
-        </p>
-        </div>
+        <>
+        <AccessDeniedHeader/>
+        <Layout>
+        <S.div>
+      <Logo size={"12"} bg={"#12142e"}/>
+      <h2>Oops</h2>
+      </S.div>
+      <br/>
+        <h4>404 ERROR:</h4>
+        <br/>
+        We can't find this page, please turn back!
+        </Layout>
+        
+        </>
     )
 }
 
-export default notFound;
+const S = {}
+
+S.div = styled.div`
+margin-top: 1rem;
+position: relative;
+> h2 {
+  font-family: 'Lobster', cursive;
+  position: absolute;
+  font-size: 3rem;
+  top: 1.5rem;
+  left: 2.8rem;
+  opacity: 0.9;
+  text-shadow: 2px 5px 5px #12142e;
+}
+`;
+
