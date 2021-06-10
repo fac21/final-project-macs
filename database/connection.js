c146617250bc3da843f3a41e644fae5077d1a4d7
Craig MacLeod <c92macleod@gmail.com>
2021-06-10 16:54:14 +0100

    remove con log

--- database/connection.js
+++ database/connection.js
@@ -8,12 +8,8 @@ const DB_URL = process.env.DATABASE_URL;
 if (!DB_URL) throw new Error("DATABASE_URL env var is not defined");
 
 const options = {
-  connectionString: DB_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+connectionString: DB_URL
 };
 
 const db = new pg.Pool(options);
-
 module.exports = db;
