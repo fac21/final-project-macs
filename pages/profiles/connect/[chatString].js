8ed776fb522d5f578a3611d8881ea4b9857fa803
Craig MacLeod <c92macleod@gmail.com>
2021-06-11 10:21:07 +0100

    dunno

--- pages/profiles/connect/[chatString].js
+++ pages/profiles/connect/[chatString].js
@@ -3,6 +3,7 @@ import Layout from "../../../components/Layout";
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const ENDPOINT = "http://127.0.0.1:4001";
 const socket = socketIOClient(ENDPOINT);
